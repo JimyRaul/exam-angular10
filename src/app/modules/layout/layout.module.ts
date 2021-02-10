8edaf057a67b81bb6abd7989631a00ac1eb0d353
JimyRaul <jcastrejonm@gmail.com>
2021-02-10 11:02:36 -0500

    feat(1d): Crear el Layout según las especificaciones del wireframe

--- src/app/modules/layout/layout.module.ts
+++ src/app/modules/layout/layout.module.ts
@@ -3,11 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { LayoutRoutingModule } from './layout-routing.module';
 import { LayoutComponent } from './pages/layout/layout.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SuscribeFormComponent } from './components/suscribe-form/suscribe-form.component';
 
 
 @NgModule({
   declarations: [
     LayoutComponent,
+    FooterComponent,
+    HeaderComponent,
+    SuscribeFormComponent,
   ],
   imports: [
     CommonModule,
